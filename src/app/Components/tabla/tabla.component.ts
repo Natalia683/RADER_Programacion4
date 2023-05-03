0db095c28aad7ec0df32e62b335373f5196b7cb6
Natalia683 <natalia.narvaez683@gmail.com>
2023-05-02 19:44:25 -0500

    cambios de tabla t.s

--- src/app/Components/tabla/tabla.component.ts
+++ src/app/Components/tabla/tabla.component.ts
@@ -1,8 +1,10 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, Input, SimpleChanges} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { TablaDataSource, TablaItem } from './tabla-datasource';
+import { ApiService } from 'src/app/Services/api.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tabla',
@@ -10,21 +12,45 @@ import { TablaDataSource, TablaItem } from './tabla-datasource';
   styleUrls: ['./tabla.component.css']
 })
 export class TablaComponent implements AfterViewInit {
+
+  @Input() dataSource: MatTableDataSource<any>;
+  displayedColumns: string[];
+  @Input() data: any[]
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<TablaItem>;
-  dataSource: TablaDataSource;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
 
-  constructor() {
-    this.dataSource = new TablaDataSource();
+  constructor(public api: ApiService) {
+    this.dataSource = new MatTableDataSource();
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    console.log(this.data)
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['data'] && this.data) {
+      this.loadTable();
+    }
+  }
+  public loadTable() {
+    this.displayedColumns = [];
+    let objeto = this.data[0];
+
+    for (let nombre of Object.keys(objeto)) {
+      this.displayedColumns.push(nombre);
+    }
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
 }
