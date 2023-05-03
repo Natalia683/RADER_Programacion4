567f31df153ad59371c044197fcedf62297d6e0b
Alexander Caicedo <dinoreygiova2001@hotmail.com>
2023-05-02 21:17:19 -0500

    Tablas 2

--- src/app/Components/historial/historial.component.ts
+++ src/app/Components/historial/historial.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from 'src/app/Services/api.service';
-import { TableService } from 'src/app/Services/table.service';
 
 @Component({
   selector: 'app-historial',
@@ -15,7 +14,7 @@ export class HistorialComponent implements OnInit{
   dataSource: MatTableDataSource<any>;
   data:any []
 
-  constructor(public api: ApiService, public tableService: TableService) {
+  constructor(public api: ApiService) {
     this.dataSource = new MatTableDataSource();
   }
 
@@ -26,24 +25,8 @@ export class HistorialComponent implements OnInit{
   public async GetHistorial() {
     this.api.Get('Historials').then((res) => {
       this.dataSource.data = res;
+      this.data = res;
     });
   }
-
-  public loadTable(data: any[]) {
-    this.displayedColumns = [];
-    let objeto = data[0];
-
-    for(let nombre of Object.keys(objeto)) {
-      this.displayedColumns.push(nombre);
-    }
-  }
-
   applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
 }
--- src/app/Components/pqr/pqr.component.ts
+++ src/app/Components/pqr/pqr.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { ApiService } from 'src/app/Services/api.service';
-import { TableService } from 'src/app/Services/table.service';
 
 @Component({
   selector: 'app-pqr',
@@ -11,12 +10,13 @@ import { TableService } from 'src/app/Services/table.service';
 
 
 export class PQRComponent implements OnInit{
+  
   displayedColumns: string[] ;
 
   dataSource: MatTableDataSource<any>;
   data:any []
 
-  constructor(public api: ApiService, public tableService: TableService) {
+  constructor(public api: ApiService) {
     this.dataSource = new MatTableDataSource();
   }
 
@@ -28,25 +28,6 @@ export class PQRComponent implements OnInit{
     this.api.Get('Solicituds').then((res) => {
       this.data = res;
       this.dataSource.data = res;
-      this.tableService.responseTable=res;
     });
-  }
-
-  public loadTable(data: any[]) {
-    this.displayedColumns = [];
-    let objeto = data[0];
-
-    for(let nombre of Object.keys(objeto)) {
-      this.displayedColumns.push(nombre);
-    }
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
   }
 }
