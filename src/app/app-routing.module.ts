84276a36881ed351df36a8645972574bfc2ae2d9
Natalia683 <natalia.narvaez683@gmail.com>
2023-05-09 16:36:18 -0500

    paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,13 @@ import { LoginComponent } from './Components/login/login.component';
 import { MantenimientoComponent } from './Components/mantenimiento/mantenimiento.component';
 import { PQRComponent } from './Components/pqr/pqr.component';
 import { TablaComponent } from './Components/tabla/tabla.component';
+import { FormularioClienteComponent } from './Form/Formulario_Cliente/formulario-cliente/formulario-cliente.component';
+import { FormularioDashboardComponent } from './Form/Formulario_Dashboard/formulario-dashboard/formulario-dashboard.component';
+import { FormularioHistorialComponent } from './Form/Formulario_Historial/formulario-historial/formulario-historial.component';
+import { FormularioInventarioComponent } from './Form/Formulario_Inventario/formulario-inventario/formulario-inventario.component';
+import { FormularioMantenimientoComponent } from './Form/Formulario_Mantenimiento/formulario-mantenimiento/formulario-mantenimiento.component';
+import { FormularioPQRComponent } from './Form/Formulario_PQR/formulario-pqr/formulario-pqr.component';
+
 
 const routes: Routes = [
 {path: "Cliente",component:ClienteComponent},
@@ -20,6 +27,12 @@ const routes: Routes = [
 {path: "Form",component:FormComponent},
 {path: "Tabla",component:TablaComponent},
 {path: "Login",component:LoginComponent},
+{path:"Formulario_Cliente",component:FormularioClienteComponent},
+{path:"Formulario_Dashboard",component:FormularioDashboardComponent},
+{path:"Formulario_historial",component:FormularioHistorialComponent},
+{path:"Formulario_Inventario",component:FormularioInventarioComponent},
+{path:"Formulario_Mantenimiento",component:FormularioMantenimientoComponent},
+{path:"Formulario_PQR",component:FormularioPQRComponent},
 
 
 ];
