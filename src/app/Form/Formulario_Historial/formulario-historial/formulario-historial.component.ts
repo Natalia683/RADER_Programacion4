53d34c8f23f90f9736f5058a6e2ec496628864d8
Natalia683 <natalia.narvaez683@gmail.com>
2023-05-09 21:36:22 -0500

    Formulario 3

--- src/app/Form/Formulario_Historial/formulario-historial/formulario-historial.component.ts
+++ src/app/Form/Formulario_Historial/formulario-historial/formulario-historial.component.ts
@@ -6,88 +6,48 @@ import { FormBuilder, Validators } from '@angular/forms';
   templateUrl: './formulario-historial.component.html',
   styleUrls: ['./formulario-historial.component.css']
 })
-export class FormularioHistorialComponent {
+   export class FormularioHistorialComponent {
+  IdHis:"";
+  Fec:"";
+  Nov:"";
+  Sug:"";
+  Inc:"";
+  Comp:"";
+  Usu:"";
+
   addressForm = this.fb.group({
-    company: null,
-    firstName: [null, Validators.required],
-    lastName: [null, Validators.required],
-    address: [null, Validators.required],
-    address2: null,
-    city: [null, Validators.required],
-    state: [null, Validators.required],
-    postalCode: [null, Validators.compose([
-      Validators.required, Validators.minLength(5), Validators.maxLength(5)])
-    ],
-    shipping: ['free', Validators.required]
+    
+    IdHistorial: [null, Validators.required],
+    FechaH: [null, Validators.required],
+    NovedadH: [null, Validators.required],
+    SugerenciausuarioH: [null, Validators.required],
+    IncidenciasH: [null, Validators.required],
+    ComponenteH: [null, Validators.required],
+    UsarioH:[null, Validators.required], 
+
   });
 
   hasUnitNumber = false;
 
-  states = [
-    {name: 'Alabama', abbreviation: 'AL'},
-    {name: 'Alaska', abbreviation: 'AK'},
-    {name: 'American Samoa', abbreviation: 'AS'},
-    {name: 'Arizona', abbreviation: 'AZ'},
-    {name: 'Arkansas', abbreviation: 'AR'},
-    {name: 'California', abbreviation: 'CA'},
-    {name: 'Colorado', abbreviation: 'CO'},
-    {name: 'Connecticut', abbreviation: 'CT'},
-    {name: 'Delaware', abbreviation: 'DE'},
-    {name: 'District Of Columbia', abbreviation: 'DC'},
-    {name: 'Federated States Of Micronesia', abbreviation: 'FM'},
-    {name: 'Florida', abbreviation: 'FL'},
-    {name: 'Georgia', abbreviation: 'GA'},
-    {name: 'Guam', abbreviation: 'GU'},
-    {name: 'Hawaii', abbreviation: 'HI'},
-    {name: 'Idaho', abbreviation: 'ID'},
-    {name: 'Illinois', abbreviation: 'IL'},
-    {name: 'Indiana', abbreviation: 'IN'},
-    {name: 'Iowa', abbreviation: 'IA'},
-    {name: 'Kansas', abbreviation: 'KS'},
-    {name: 'Kentucky', abbreviation: 'KY'},
-    {name: 'Louisiana', abbreviation: 'LA'},
-    {name: 'Maine', abbreviation: 'ME'},
-    {name: 'Marshall Islands', abbreviation: 'MH'},
-    {name: 'Maryland', abbreviation: 'MD'},
-    {name: 'Massachusetts', abbreviation: 'MA'},
-    {name: 'Michigan', abbreviation: 'MI'},
-    {name: 'Minnesota', abbreviation: 'MN'},
-    {name: 'Mississippi', abbreviation: 'MS'},
-    {name: 'Missouri', abbreviation: 'MO'},
-    {name: 'Montana', abbreviation: 'MT'},
-    {name: 'Nebraska', abbreviation: 'NE'},
-    {name: 'Nevada', abbreviation: 'NV'},
-    {name: 'New Hampshire', abbreviation: 'NH'},
-    {name: 'New Jersey', abbreviation: 'NJ'},
-    {name: 'New Mexico', abbreviation: 'NM'},
-    {name: 'New York', abbreviation: 'NY'},
-    {name: 'North Carolina', abbreviation: 'NC'},
-    {name: 'North Dakota', abbreviation: 'ND'},
-    {name: 'Northern Mariana Islands', abbreviation: 'MP'},
-    {name: 'Ohio', abbreviation: 'OH'},
-    {name: 'Oklahoma', abbreviation: 'OK'},
-    {name: 'Oregon', abbreviation: 'OR'},
-    {name: 'Palau', abbreviation: 'PW'},
-    {name: 'Pennsylvania', abbreviation: 'PA'},
-    {name: 'Puerto Rico', abbreviation: 'PR'},
-    {name: 'Rhode Island', abbreviation: 'RI'},
-    {name: 'South Carolina', abbreviation: 'SC'},
-    {name: 'South Dakota', abbreviation: 'SD'},
-    {name: 'Tennessee', abbreviation: 'TN'},
-    {name: 'Texas', abbreviation: 'TX'},
-    {name: 'Utah', abbreviation: 'UT'},
-    {name: 'Vermont', abbreviation: 'VT'},
-    {name: 'Virgin Islands', abbreviation: 'VI'},
-    {name: 'Virginia', abbreviation: 'VA'},
-    {name: 'Washington', abbreviation: 'WA'},
-    {name: 'West Virginia', abbreviation: 'WV'},
-    {name: 'Wisconsin', abbreviation: 'WI'},
-    {name: 'Wyoming', abbreviation: 'WY'}
-  ];
 
   constructor(private fb: FormBuilder) {}
 
-  onSubmit(): void {
-    alert('Thanks!');
-  }
+
+  async onSubmit(){
+    this.IdHis=this.addressForm.controls["IdHistorial"].value;
+    this.Fec=this.addressForm.controls["FechaH"].value;
+    this.Nov=this.addressForm.controls["NovedadH"].value;
+    this.Sug=this.addressForm.controls["SugerenciausuarioH"].value;
+    this.Inc=this.addressForm.controls["IncidenciasH"].value;
+    this.Comp=this.addressForm.controls["ComponenteH"].value;
+    this.Usu=this.addressForm.controls["UsuarioH"].value;
+
+    console.log(this.IdHis);
+    console.log(this.Fec);
+    console.log(this.Nov);
+    console.log(this.Sug);
+    console.log(this.Inc);
+    console.log(this.Comp);
+    console.log(this.Usu);
+  }  
 }
